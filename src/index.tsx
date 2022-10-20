774a06a4f810d329d0f7e374b24c12c31f5df186
Alamgir55 <alamgir555hossain@gmail.com>
2022-10-20 05:29:23 -0400

    'css-for-cells-redux-hooks'

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import ReactDOM from 'react-dom/client';
-//import CodeCell from './components/code-cell';
-import TextEditor from './components/text-editor';
 import { Provider } from 'react-redux';
 import { store } from './state';
+import CellList from './components/cell-list'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -13,7 +13,7 @@ const App = () => {
   return (
     <Provider store={store}>
       <div>
-        <TextEditor />
+        <CellList />
       </div>
     </Provider>
   );
